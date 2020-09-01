4d817297447f186e54397dcac34f3ab2840026be
omakaroni <jon_resell_3@hotmail.com>
2020-09-01 13:13:49 +0200

    I have made some changes

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -1,6 +1,24 @@
+/*
+This is a class in programming
+this green text wont run as a code you can also use //
+"\n" or std::endl; = ending a line
+\t = tabs out the line when running
+*/
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int age = 29;
+bool sw = false;
+char initial = 'J';
+std::string name = "Jon";
+double pi = 3.14;
+
+int main() {
+
+	std::cout << "Enter your age :";
+	std::cin >> age;
+	std::cout << "Your age is : " << age << "\n";
+	std::cout << "You are young ";
+	std::cout << "s";
+
 }
