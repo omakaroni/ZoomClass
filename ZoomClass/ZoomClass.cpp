1c96a727b3739eb762b3cc4094a5408d6d496734
omakaroni <jon_resell_3@hotmail.com>
2020-09-01 13:55:59 +0200

    done

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -6,19 +6,43 @@ this green text wont run as a code you can also use //
 */
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 int age = 29;
 bool sw = false;
 char initial = 'J';
-std::string name = "Jon";
+std::string Fullname = "";
 double pi = 3.14;
+int A = 10;
+int B = 5;
+int C = 20;
+int Z = 10;
 
 int main() {
+	system("cls");
 
-	std::cout << "Enter your age :";
-	std::cin >> age;
-	std::cout << "Your age is : " << age << "\n";
-	std::cout << "You are young ";
-	std::cout << "s";
+	if (A > B) {
+		std::cout << "A is greater than B\n";
+	}
+	else {
+		std::cout << "B is greater than A\n";
+	}
+	if (A == Z) {
+		std::cout << "A and Z are equal\n";
+	}
+	if (A >= Z) { // if ( a > z || a == z )
+		std::cout << "Nothing happens here \n";
+	}
+	if (A != B) {
+		std::cout << "A and B are not equal\n";
+	}
 
+	// I add a comment here
+	std::cout << "Enter your Name :";
+	std::getline(std::cin, Fullname);
+	std::cout << "Your full name is :" << Fullname;
+	// std::cout << "Your age is : " << age << "\n";
+	// std::cout << "You are young ";
+	_getch();
+	system("pause");
 }
